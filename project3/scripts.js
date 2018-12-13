7328b15395def2bc76742986b5dec266d82ed9e6
Will Sabin <willsabin@Wills-MacBook-Pro.local>
2018-12-13 13:21:27 -0500

    Finished project3

--- project3/scripts.js
+++ project3/scripts.js
@@ -1,19 +1,18 @@
 function area() {
-    var height = parseInt(document.getElementById("width").value);
-    var width = parseInt(document.getElementById("height").value);
+    var height = parseInt(document.getElementById("height").value);
+    var width = parseInt(document.getElementById("width").value);
 
     var result = height * width;
 
     document.getElementById("result").innerHTML = result;
 }
-function
 
 function cubed() {
   var number = parseInt(document.getElementById("number").value);
 
-  var result2 = number * number * number
+  var result2 = number * number * number;
 
-  document.getElementById("result 2").innerHTML = result2
+  document.getElementById("result2").innerHTML = result2;
 
 }
 
@@ -21,10 +20,7 @@ function Tri(){
   var base  = parseInt(document.getElementById("base").value);
   var height2 = parseInt(document.getElementById("height2").value);
 
-  var result3 = (base * height2)/ 2
+  var result3 = (base * height2)/ 2;
 
-  document.getElementById("result 3").innerHTML = result3
-
-
-
+  document.getElementById("result3").innerHTML = result3;
 }
